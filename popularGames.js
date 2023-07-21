bf1c19393562f768b140fb7dac887cc90cf72af8
CoderDude1232 <100014515+CoderDude1232@users.noreply.github.com>
2023-07-22 06:29:14 +0800

    Create popularGames.js

---
+++ popularGames.js
@@ -0,0 +1,33 @@
+const popularGames = [
+    {
+        name: 'Among Us',
+        image: 'among_us.jpg',
+        downloadLink: 'https://store.steampowered.com/app/945360/Among_Us/'
+    },
+    {
+        name: 'Minecraft',
+        image: 'minecraft.jpg',
+        downloadLink: 'https://www.minecraft.net/en-us/store/minecraft-java-edition'
+    },
+    {
+        name: 'Call of Duty: Warzone',
+        image: 'cod_warzone.jpg',
+        downloadLink: 'https://www.callofduty.com/warzone/download'
+    },
+    {
+        name: 'Fall Guys',
+        image: 'fall_guys.jpg',
+        downloadLink: 'https://store.steampowered.com/app/1097150/Fall_Guys_Ultimate_Knockout/'
+    },
+    {
+        name: 'Roblox',
+        image: 'roblox.jpg',
+        downloadLink: 'https://www.roblox.com/'
+    },
+    {
+        name: 'Fortnite',
+        image: 'fortnite.jpg',
+        downloadLink: 'https://www.epicgames.com/fortnite/en-US/home'
+    }
+    // Continue adding more popular games...
+];
