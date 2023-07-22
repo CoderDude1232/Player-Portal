f2b5453185ccf55ea4bfb2f4e323df28028c1acc
CoderDude1232 <100014515+CoderDude1232@users.noreply.github.com>
2023-07-22 08:54:11 +0800

    Create popularGames.js

---
+++ popularGames.js
@@ -0,0 +1,10 @@
+const popularGames = [
+    {title: 'Minecraft', description: 'Sandbox video game where players can build and destroy different types of blocks in a 3D procedurally generated world.', thumbnail: 'minecraft.jpg'},
+    {title: 'Fortnite', description: 'Online multiplayer battle royale game where players fight to be the last person standing.', thumbnail: 'fortnite.jpg'},
+    {title: 'League of Legends', description: 'Multiplayer online battle arena video game where players assume the role of a "champion" with unique abilities.', thumbnail: 'lol.jpg'},
+    {title: 'Among Us', description: 'Online multiplayer social deduction game set in a space-themed setting.', thumbnail: 'amongus.jpg'},
+    {title: 'Call of Duty: Warzone', description: 'Free-to-play battle royale video game part of the 2019 title Call of Duty: Modern Warfare.', thumbnail: 'codwarzone.jpg'},
+    {title: 'Apex Legends', description: 'Free-to-play hero battle royale game where legendary competitors battle for glory, fame, and fortune on the fringes of the Frontier.', thumbnail: 'apexlegends.jpg'},
+    {title: 'Roblox', description: 'Online platform and storefront where users go to build online 3D games.', thumbnail: 'roblox.jpg'},
+    // Add more popular games...
+];
